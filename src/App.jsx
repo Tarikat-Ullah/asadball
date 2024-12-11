51d32eaea7b31f47d43a8ac425ecdc3c7928e3b4
anikahammedkhan <anikkhan1105@gmail.com>
2024-12-11 19:59:58 +0600

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div id="canvas">
+      <div id="ball"></div>
+      <div id="instructions">Click to launch the ball!</div>
+    </div>
+  );
+};
+
+export default App;
